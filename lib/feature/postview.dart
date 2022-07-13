583e7b7dec1c7dccfec1189079013bd3f6a68f83
Fırat Şakar <frtskr0@gmail.com>
2022-07-13 10:41:01 +0300

    display and service contact

--- lib/feature/postview.dart
+++ lib/feature/postview.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_state_management_second_project/feature/model/post_model.dart';
+import 'package:flutter_state_management_second_project/feature/service/post_service.dart';
 
 class PostPage extends StatefulWidget {
   const PostPage({Key? key}) : super(key: key);
@@ -10,8 +10,41 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  //Verilerimizi çekmek için servisimizi tanımladık.
+  final PostService _postService = PostService();
+  //Veriler için bir liste değişkeni oluşturduk.
+  List<PostModel>? _postModel;
+  //Servis katmanından veriler yüklenirken durum kontrolü için bir değişken tanımladık.
+  //Bu değişkeni progress bar için kullanacağız.
+  bool isLoading = false;
+
+//Fonksiyonumuzda isLoading değşkenimizin değişimini kontrol ediyoruz.
+  void changeLoading() {
+    //Yapılan değişikliği ekrana da bildirmek için setState metodunu kullanıyoruz.
+    setState(() {
+      isLoading = !isLoading;
+    });
+  }
+
+//verileri _postModel içerisine servisten çekecek olan metodumuz.
+  Future<void> init() async {
+    _postModel = await _postService.fetchPost();
+    //Veriler çekildiyse isLoading değişken değerini değiştirdik.
+    changeLoading();
+  }
+
+//??
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+//Veriler yüklendiyse ListView builder ile ekranımıza verilerimiz oluşturulacak.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
